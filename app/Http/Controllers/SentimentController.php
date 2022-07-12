ce5c1c22582ee55ef7e816cc295d123feaa4be5d
Sandeep Kaur <sandeepkaur@founderandlightning.com>
2022-07-12 17:31:06 +0530

    Update SentimentController.php

--- app/Http/Controllers/SentimentController.php
+++ app/Http/Controllers/SentimentController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Jobs\FetchSentiments;
+use Google\Cloud\Language\LanguageClient;
 
 class SentimentController extends Controller
 {
     public function index()
     {
         echo "running jobs";
-        
-        FetchSentiments::dispatchSync();
+        $language = new LanguageClient();
+        $annotation = $language->analyzeSentiment("Hello world");
+        $sentiment = $annotation->sentiment();
+        echo $sentiment['score'];
+        echo $sentiment['magnitude'];
+        //FetchSentiments::dispatchSync();
     }
 }
